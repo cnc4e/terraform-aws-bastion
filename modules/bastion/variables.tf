d5b1a41ec0ebca72f70dd14a4e1950dfa23ee7d4
kato.kenta <kato.kenta@tis.co.jp>
2025-04-25 03:14:27 +0000

    code

---
+++ modules/bastion/variables.tf
@@ -0,0 +1,34 @@
+variable "resouce_name" {
+  description = "Requested instance type"
+  type        = string
+}
+
+variable "instance_type" {
+  description = "Requested instance type"
+  type        = string
+}
+
+variable "start_time" {
+  description = "Time to start up the bastion server"
+  type        = string
+}
+
+variable "stop_time" {
+  description = "Time to stop the bastion server"
+  type        = string
+}
+
+variable "timezone" {
+  description = "Time zone in which the user is located"
+  type        = string
+}
+
+variable "vpc_id" {
+  description = "VPC ID where the bastion server is located"
+  type        = string
+}
+
+variable "subnet_id" {
+  description = "Subnet ID where the bastion server is located"
+  type        = string
+}
