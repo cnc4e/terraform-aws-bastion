658a27c6437906d532e01b0112be6a241815ef42
kato.kenta <kato.kenta@tis.co.jp>
2025-06-05 08:29:03 +0000

    #7 auto-optional

--- modules/bastion/schedule.tf
+++ modules/bastion/schedule.tf
@@ -23,6 +23,7 @@ resource "aws_scheduler_schedule" "bastion_start" {
 # Stop
 resource "aws_scheduler_schedule" "bastion_stop" {
   count = can(regex("cron", var.stop_time)) ? 1 : 0
+  
   name                         = "${var.resouce_name}-bastion-schedule-stop"
   schedule_expression          = var.stop_time
   schedule_expression_timezone = var.timezone
