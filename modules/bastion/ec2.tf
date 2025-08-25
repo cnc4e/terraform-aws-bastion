a3c4268b3eeff6f858d3b65d7dfc415f0c7f8c59
suzuki_shota <suzuki.shota@tis.co.jp>
2025-08-25 07:42:32 +0000

    fmtおよびREADMEの追記

--- modules/bastion/ec2.tf
+++ modules/bastion/ec2.tf
@@ -9,12 +9,12 @@ data "aws_ami" "this" {
 }
 
 resource "aws_instance" "this" {
-  ami                    = data.aws_ami.this.id
-  instance_type          = var.instance_type
-  subnet_id              = var.subnet_id != "" ? var.subnet_id : aws_subnet.public[0].id
-  availability_zone      = var.availability_zone != "" ? var.availability_zone : null
-  iam_instance_profile   = aws_iam_instance_profile.session_manager.name
-  vpc_security_group_ids = [aws_security_group.ec2.id]
+  ami                     = data.aws_ami.this.id
+  instance_type           = var.instance_type
+  subnet_id               = var.subnet_id != "" ? var.subnet_id : aws_subnet.public[0].id
+  availability_zone       = var.availability_zone != "" ? var.availability_zone : null
+  iam_instance_profile    = aws_iam_instance_profile.session_manager.name
+  vpc_security_group_ids  = [aws_security_group.ec2.id]
   disable_api_termination = var.disable_api_termination
 
   user_data = file("${path.module}/script/userdata.sh")
