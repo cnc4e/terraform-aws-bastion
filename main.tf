f5a3373241d3e42a97b1e822e465f0ea4bb626e4
suzuki_shota <suzuki.shota@tis.co.jp>
2025-08-25 04:21:04 +0000

    タイムゾーンをAsia/Tokyoに固定する

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ module "bastion" {
   instance_type     = var.instance_type
   start_time        = var.start_time
   stop_time         = var.stop_time
-  timezone          = var.timezone
+  #timezone          = var.timezone
   vpc_id            = var.vpc_id
   subnet_id         = var.subnet_id
   generation        = var.generation
