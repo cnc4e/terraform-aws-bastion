9be8f924f3efdb2aee60401877ca29540ba529ab
suzuki_shota <suzuki.shota@tis.co.jp>
2025-08-26 07:44:52 +0000

    test

--- modules/bastion/ec2.tf
+++ modules/bastion/ec2.tf
@@ -15,7 +15,7 @@ resource "aws_instance" "this" {
   availability_zone       = var.availability_zone != "" ? var.availability_zone : null
   iam_instance_profile    = aws_iam_instance_profile.session_manager.name
   vpc_security_group_ids  = [aws_security_group.ec2.id]
-  disable_api_termination = var.disable_api_termination
+  disable_api_termination =     var.disable_api_termination
 
   user_data = file("${path.module}/script/userdata.sh")
 
